26ce3715e959c6574e8d309c9cfa6a41069377b1
lf0205 <lf.0205@qq.com>
2019-10-26 14:35:13 +0800

    'lf'

---
+++ 36Kr-app/pages/index/components/IndexRecommend.vue
@@ -0,0 +1,59 @@
+<template>
+	<view class="">
+		<view class="uni-padding-wrap">
+			<view class="page-section swiper">
+				<view class="page-section-spacing">
+					<swiper class="swiper" :indicator-dots="indicatorDots" :autoplay="autoplay" :interval="interval" :duration="duration">
+						<swiper-item v-for="(item,index) in bannerList" :key='index'>
+							<view class="swiper-item uni-bg-red">
+								<image :src="item.templateMaterial.widgetImage" mode=""></image>
+							</view>
+						</swiper-item>
+					</swiper>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+<script>
+	export default{
+		data(){
+			return{
+				bannerList:[],
+				background: ['color1', 'color2', 'color3'],
+				indicatorDots: true,
+				autoplay: true,
+				interval: 2000,
+				duration: 500
+			}
+		},
+		onLoad() {
+			console.log(1)
+			uni.request({
+				url: 'http://rap2api.taobao.org/app/mock/234738/get/home/recommend/carousel',
+				method: 'GET',
+				success: res => {
+					console.log(res.data.data.bannerList)
+					this.bannerList = res.data.data.bannerList
+				},
+			});
+		},
+		methods:{
+			changeIndicatorDots(e) {
+				this.indicatorDots = !this.indicatorDots
+			},
+			changeAutoplay(e) {
+				this.autoplay = !this.autoplay
+			},
+			intervalChange(e) {
+				this.interval = e.target.value
+			},
+			durationChange(e) {
+				this.duration = e.target.value
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
--- 36Kr-app/pages/index/index.vue
+++ 36Kr-app/pages/index/index.vue
@@ -1,6 +1,9 @@
 <template>
-	<view>
-		index
+	<view class="Index">
+		<view class="IndexSearch">
+			<view class="SignIn iconfont icon-Sign"></view>
+			<view class="IndexFrom iconfont icon-sousuo">大家都在搜：淘集集</view>
+		</view>
 	</view>
 </template>
 
@@ -16,10 +19,38 @@
 		},
 		methods: {
 
+		},
+		components:{
+			
 		}
 	}
 </script>
 
 <style>
-	
+	/* 首页主页 */
+	.Index{
+		height: 100%;
+		padding: 0 25upx;
+		background: #FFFFF;
+	}
+	/* 首页头部搜索 */
+	.IndexSearch{
+		padding: 10upx 0;
+		display: flex;
+		align-items: center;
+		justify-content: space-around;
+	}
+	.IndexSearch>.SignIn{
+		font-size: 34upx;
+	}
+	.IndexSearch>.IndexFrom{
+		background: #fbfbfb;
+		font-size: 12upx;
+		display: inline-block;
+		font-size: 26upx;
+		width: 90%;
+		text-align: center;
+		padding:10upx 0;
+		color: #898989;
+	}
 </style>
