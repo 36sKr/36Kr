5e31de84750a5355bb2d943f5b3941c8b8a5e9f0
lf0205 <lf.0205@qq.com>
2019-10-28 10:16:12 +0800

    '10.28第二次'

--- 36Kr-app/pages/components/IndexListPages.vue
+++ 36Kr-app/pages/components/IndexListPages.vue
@@ -9,7 +9,13 @@
 			<view class="">{{PagesData.templateMaterial.authorName}}</view>			
 			<view class="">{{PagesData.templateMaterial.widgetTitle}}</view>
 		</view>
-					
+		<view class="IndexListPagesFooter">
+			<view class="IndexListPagesFooterInput">有何高见，展开讲讲</view>
+			<view class="IndexListPagesFooterInfo iconfont icon-pinglun"></view>
+			<view class="IndexListPagesFooterDz iconfont icon-changyongtubiao-xianxingdaochu-zhuanqu-1"></view>
+			<view class="IndexListPagesFooterSc iconfont icon-xingxing"></view>
+			<view class="IndexListPagesFooterFx iconfont icon-fenxiang"></view>
+		</view>
 	</view>
 </template>
 
@@ -58,5 +64,23 @@
 	}
 	.IndexListPagesInfo{
 		margin-top: 100upx;		
+	}
+	.IndexListPagesFooter{
+		position: fixed;
+		bottom: 0;
+		left: 0;
+		background: #fff;
+		width: 100%;
+		height: 100upx;
+		align-items: center;
+		justify-content: space-around;
+		border-top: 1px solid #EEEEEE;
+		display: flex;
+	}
+	.IndexListPagesFooterInput{
+		padding: 15upx;
+		background: #EEEEEE;
+		font-size: 12upx;
+		color: #999999;
 	}
 </style>
